bfad423e60b695040065bea2eba5009ae5ca5a69
JucaRei <32142926+JucaRei@users.noreply.github.com>
2021-09-22 23:57:10 -0300

    Update LXDE.sh

--- LXDE.sh
+++ LXDE.sh
@@ -24,7 +24,7 @@ makepkg -si --noconfirm
 # sudo snap install beekeeper-studio postbird
 
 # LXDE
-sudo pikaur -S xorg lxde lxdm tilix leafpad gtkmm3 ttf-dejavu ttf-liberation vlc xed xreader metacity gnome-shell firefox-developer-edition ttf-consolas-ligaturized ttf-fira-code ttf-jetbrains-mono font-victor-mono breeze-hacked-cursor-theme
+sudo pikaur -S xorg lxde lxdm tilix ttf-dejavu ttf-liberation vlc picom firefox-developer-edition ttf-consolas-ligaturized ttf-fira-code ttf-jetbrains-mono font-victor-mono breeze-hacked-cursor-theme
 
 # sudo cp /etc/x11/xinit/xinitrc ~/.xinitrc
 
