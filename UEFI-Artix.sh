cadd01dc82b18b931f3eebbe281e924ece3b0f19
Reinaldo <reinaldo800@gmail.com>
2021-11-07 22:08:32 -0300

    Fix

--- UEFI-Artix.sh
+++ UEFI-Artix.sh
@@ -16,6 +16,8 @@ echo root:200291 | chpasswd
 
 pacman -S grub grub-btrfs efibootmgr networkmanager networkmanager-runit network-manager-applet dialog artix-archlinux-support wpa_supplicant wpa_supplicant-runit pacman-contrib avahi avahi-runit xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-runit bluez-utils pulseaudio-bluetooth pulseaudio-alsa pulseaudio-equalizer pulseaudio-jack alsa-utils bash-completion exfat-utils cups cups-runit hplip openssh openssh-runit rsync rsync-runit acpi acpid acpi_call tlp tlp-runit virt-manager qemu qemu-guest-agent-runit qemu-arch-extra vde2 edk2-ovmf bridge-utils dnsmasq dnsmasq-runit vde2 ebtables openbsd-netcat iptables-nft ipset firewalld firewalld-runit flatpak sof-firmware nss-mdns acpid-runit os-prober ntfs-3g
 
+# ADD Repos
+
 echo "[extra]" >>/etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist-arch" >>/etc/pacman.conf
 
@@ -39,7 +41,8 @@ pacman-key --populate archlinux
 
 pacman -S xf86-video-intel
 
-#pacman -S nvidia nvidia-utils nvidia-settings
+# pacman -S reflector
+# pacman -S nvidia-lts nvidia-utils nvidia-settings
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
