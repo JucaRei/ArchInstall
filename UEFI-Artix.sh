b6f944c88212c9c8d7184563b6f02fee487be834
Reinaldo <reinaldo800@gmail.com>
2021-11-13 16:30:03 -0300

    Pequenas alterações

--- UEFI-Artix.sh
+++ UEFI-Artix.sh
@@ -25,7 +25,7 @@ echo "[community]" >>/etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist-arch" >>/etc/pacman.conf
 
 echo "[multilib]" >>/etc/pacman.conf
-echo "Include = /etc/pacman.d/mirrorlist-arch" >>/etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist-arch" >>/ehwtc/pacman.conf
 
 echo "[universe]" >>/etc/pacman.conf
 echo "Server = https://universe.artixlinux.org/$arch" >>/etc/pacman.conf
@@ -40,7 +40,6 @@ echo "Server = http://omniverse.artixlinux.org/$arch" >>/etc/pacman.conf
 pacman-key --populate archlinux
 
 pacman -S grub grub-btrfs efibootmgr networkmanager networkmanager-runit network-manager-applet ntp ntp-runit dialog wpa_supplicant wpa_supplicant-runit pacman-contrib avahi avahi-runit xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-runit bluez-utils pulseaudio-bluetooth pulseaudio-alsa pulseaudio-equalizer pulseaudio-jack alsa-utils alsa-utils-runit bash-completion exfat-utils cups cups-runit hplip openssh openssh-runit rsync rsync-runit acpi acpid acpi_call tlp tlp-runit virt-manager qemu qemu-guest-agent-runit qemu-arch-extra vde2 edk2-ovmf bridge-utils dnsmasq dnsmasq-runit vde2 ebtables openbsd-netcat iptables-nft ipset firewalld firewalld-runit flatpak sof-firmware nss-mdns acpid-runit os-prober ntfs-3g
-
 
 # pacman -S xf86-video-intel xorg --ignore xorg-server-xdmx
 
