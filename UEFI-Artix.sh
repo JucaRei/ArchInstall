42d8b9158f346a012cbe89c6ec46329a42ab2a61
JucaRei <32142926+JucaRei@users.noreply.github.com>
2021-11-14 00:00:52 -0300

    Update UEFI-Artix.sh

--- UEFI-Artix.sh
+++ UEFI-Artix.sh
@@ -43,12 +43,14 @@ pacman-key --populate archlinux
 
 pacman -Syyy
 
-pacman -S grub grub-btrfs efibootmgr networkmanager networkmanager-runit network-manager-applet ntp ntp-runit dialog wpa_supplicant wpa_supplicant-runit pacman-contrib avahi avahi-runit xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-runit bluez-utils pulseaudio-bluetooth pulseaudio-alsa pulseaudio-equalizer pulseaudio-jack alsa-utils alsa-utils-runit bash-completion exfat-utils cups cups-runit hplip openssh openssh-runit rsync rsync-runit acpi acpid acpi_call tlp tlp-runit virt-manager libvirt-runit qemu qemu-guest-agent-runit qemu-arch-extra vde2 edk2-ovmf bridge-utils dnsmasq dnsmasq-runit vde2 ebtables openbsd-netcat iptables-nft ipset firewalld firewalld-runit flatpak sof-firmware nss-mdns acpid-runit os-prober ntfs-3g
+pacman -S grub grub-btrfs efibootmgr networkmanager networkmanager-runit network-manager-applet ntp ntp-runit dialog wpa_supplicant rsm wpa_supplicant-runit pacman-contrib avahi avahi-runit xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-runit bluez-utils pulseaudio-bluetooth pulseaudio-alsa pulseaudio-equalizer pulseaudio-jack alsa-utils alsa-utils-runit bash-completion exfat-utils cups cups-runit hplip openssh openssh-runit rsync rsync-runit acpi acpid acpi_call tlp tlp-runit virt-manager libvirt-runit qemu qemu-guest-agent-runit qemu-arch-extra vde2 edk2-ovmf bridge-utils dnsmasq dnsmasq-runit vde2 ebtables openbsd-netcat iptables-nft ipset firewalld firewalld-runit flatpak sof-firmware nss-mdns acpid-runit os-prober ntfs-3g 
 
 # pacman -S xf86-video-intel xorg --ignore xorg-server-xdmx
 
 # pacman -S reflector
 # pacman -S nvidia-lts nvidia-utils nvidia-settings
+
+# pacman -S zramen zramen-runit
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
