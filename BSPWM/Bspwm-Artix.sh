9b7847be16ec68ca6dbefc4fe39d3f805480ea97
Reinaldo <reinaldo800@gmail.com>
2021-11-19 20:54:47 -0300

    Fix

--- BSPWM/Bspwm-Artix.sh
+++ BSPWM/Bspwm-Artix.sh
@@ -45,7 +45,7 @@ fi
 #paru -S nvidia-lts nvidia-utils nvidia-settings
 #paru -S xorg-server xorg-xsetroot xorg-xinit libxrandr libxft xorg-xrdb libxinerama
 
-paru -S bspwm arandr xorg spacefm sxhkd polybar xfce4-terminal light-locker alacritty playerctl awesome-terminal-fonts ttf-font-awesome dmenu nitrogen feh unclutter libinput libinput-gestures picom evince-no-gnome neovim rofi dunst scrot archlinux-wallpaper lxappearance ligthdm lightdm-runit lightdm-webkit-theme-aether lightdm-webkit2-greeter lightdm-settings nerd-fonts-iosevka ttf-icomoon-feather light-locker mpd ncmpcpp mpc neofetch htop geany
+paru -S bspwm arandr xorg spacefm sxhkd polybar xfce4-terminal light-locker alacritty playerctl awesome-terminal-fonts ttf-font-awesome dmenu nitrogen feh unclutter libinput libinput-gestures picom evince-no-gnome neovim rofi dunst scrot archlinux-wallpaper lxappearance lightdm lightdm-runit lightdm-webkit-theme-aether lightdm-webkit2-greeter lightdm-settings nerd-fonts-iosevka ttf-icomoon-feather light-locker mpd ncmpcpp mpc neofetch htop geany
 
 xdg-user-dirs-update
 
