2140f5be27a4869cf8b226fdaf27349e04345376
Reinaldo <reinaldo800@gmail.com>
2022-01-13 23:13:16 -0300

    fix

--- BSPWM/Bspwm-Void.sh
+++ BSPWM/Bspwm-Void.sh
@@ -1,70 +1,77 @@
 #!/bin/bash
 
-xdg-user-dirs-update
+#xdg-user-dirs-update
 
-sudo vpm -S bspwm xorg-minimal autorandr arandr pcmanfm sxhkd glow ranger polybar light-locker rxvt-unicode rxvt-unicode-terminfo urxvt-perls playerctl font-firacode font-awesome dmenu nitrogen feh unclutter xclip libinput libinput-gestures picom evince neovim rofi dunst scrot lxappearance lightdm lightdm-webkit2-greeter font-iosevka light-locker mpd ncmpcpp mpv mpc neofetch nvtop htop geany
+sudo vpm i bspwm xorg-minimal autorandr arandr pcmanfm sxhkd glow sviv ImageMagick ranger polybar flameshot light-locker rxvt-unicode rxvt-unicode-terminfo urxvt-perls playerctl font-firacode font-awesome dmenu nitrogen feh unclutter xclip libinput libinput-gestures picom evince neovim rofi dunst scrot lxappearance lightdm lightdm-gtk3-greeter font-iosevka light-locker mpd ncmpcpp mpv mpc neofetch htop geany
 
-mkdir -p ${HOME}/Documents/workspace/{Github,Others,Customizations,Configs,Tests,Composes}
-mkdir -p ${HOME}/.local/share/fonts
-mkdir -p ${HOME}/.urxvt/ext
-mkdir -p ${HOME}/.config/{bspwm,rofi,sxhkd,dunst,polybar}
-mkdir -p ${HOME}/.config/{mpd,ncmpcpp} && cd ${HOME}/.config/mpd
+mkdir -p ~/Documents/workspace/{Github,Others,Customizations,Configs,Tests,Composes}
+mkdir -p ~/.local/share/fonts
+mkdir -p ~/.urxvt/ext
+mkdir -p ~/.config/{bspwm,rofi,sxhkd,dunst,polybar}
+mkdir -p ~/.config/{mpd,ncmpcpp} 
+cd ~/.config/mpd
 touch database mpd.conf mpd.fifo mpd.log mpdstate
 
 cd
 
-cd ${HOME}/Documents/workspace/Customizations
+cd ~/Documents/workspace/Customizations
 git clone https://github.com/JucaRei/rofi
 git clone https://github.com/JucaRei/polybar-themes
 git clone https://github.com/JucaRei/dotfiles
 git clone https://github.com/JucaRei/fonts
 
-cd ${HOME}/Documents/workspace/Configs
+cd ~/Documents/workspace/Configs
 git clone https://github.com/JucaRei/ArchInstall
 
 # Font Preview
-mkdir ${HOME}/scripts
-curl -L https://git.io/raw_fontpreview > fontpreview && chmod +x fontpreview
-echo "export PATH=$HOME/scripts/fontpreview:$PATH" >> ${HOME}/.bashrc
-source ${HOME}/.bashrc
+mkdir ~/scripts
+cd ~/scripts
+https://github.com/JucaRei/fontpreview
+cd fontpreview
+sudo make install 
+echo 'export PATH="$HOME/scripts/fontpreview:$PATH"' >> ~/.bashrc
+source ~/.bashrc
 
-cd ${HOME}/Documents/workspace/Customizations/rofi
+cd
+
+cd ~/Documents/workspace/Customizations/rofi
 chmod +x setup.sh
 ./setup.sh
 
 # Install Fonts
-cd ${HOME}/Documents/workspace/Customizations/fonts/fonts && cp *.ttf *.otf ${HOME}/.local/share/fonts
+cd ~/Documents/workspace/Customizations/fonts/fonts && cp *.ttf *.otf ~/.local/share/fonts
 fc-cache -fv
 
 # mpd e ncmpcpp
 cd
 
-cp -f ${HOME}/ArchInstall/BSPWM/mpd/mpd.conf ${HOME}/.config/mpd
-cp -f ${HOME}/ArchInstall/BSPWM/ncmpcpp/config ${HOME}/.config/ncmpcpp
+cp -f ~/Documents/workspace/Configs/ArchInstall/BSPWM/mpd/mpd.conf ~/.config/mpd
+cp -f ~/Documents/workspace/Configs/ArchInstall/BSPWM/ncmpcpp/config ~/.config/ncmpcpp
 
 # Xresources | Fehgb | Xprofile etc
 cd
-cp -f ${HOME}/Documents/workspace/Configs/Customizations/dotfiles/wallpaper.jpg ${HOME}/Pictures
-cp -f ${HOME}/Documents/workspace/Configs/Customizations/dotfiles/polybar ${HOME}/.config
-cp -f ${HOME}/Documents/workspace/Configs/ArchInstall/BSPWM/Xresources ${HOME}/.Xresources
-cp -f ${HOME}/Documents/workspace/Configs/ArchInstall/BSPWM/fehbg ${HOME}/.fehgb
-cp -f ${HOME}/Documents/workspace/Configs/ArchInstall/BSPWM/xprofile ${HOME}/.xprofile
-cp -f ${HOME}/Documents/workspace/Configs/ArchInstall/BSPWM/vtwhell ${HOME}/.urxvt/ext
-cp -f ${HOME}/Documents/workspace/Configs/ArchInstall/BSPWM/dualbsp.sh ${HOME}/scripts
-cp -f ${HOME}/Documents/workspace/Configs/ArchInstall/BSPWM/DualMonPolybar.sh ${HOME}/scripts
-sudo cp -f ${HOME}/Documents/workspace/Configs/ArchInstall/BSPWM/display-lightdm.sh /etc/lightdm
-sudo cp -f /etc/dunst/dunstrc ${HOME}/.config/dunst
+cp -f ~/Documents/workspace/Customizations/dotfiles/wallpaper.jpg ~/Pictures
+cp -rf ~/Documents/workspace/Customizations/dotfiles/polybar ~/.config
+cp -f ~/Documents/workspace/Configs/ArchInstall/BSPWM/Xresources ~/.Xresources
+cp -f ~/Documents/workspace/Configs/ArchInstall/BSPWM/fehbg ~/.fehgb
+cp -f ~/Documents/workspace/Configs/ArchInstall/BSPWM/xprofile ~/.xprofile
+cp -f ~/Documents/workspace/Configs/ArchInstall/BSPWM/vtwhell ~/.urxvt/ext
+cp -f ~/Documents/workspace/Configs/ArchInstall/BSPWM/dualbsp.sh ~/scripts
+cp -f ~/Documents/workspace/Configs/ArchInstall/BSPWM/DualMonPolybar.sh ~/scripts
+sudo cp -f ~/Documents/workspace/Configs/ArchInstall/BSPWM/display-lightdm.sh /etc/lightdm
+cp -f /etc/dunst/dunstrc ~/.config/dunst/
 install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
 install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
 
+sudo sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-gtk-greeter/g' /etc/lightdm/lightdm.conf
 
-cat << EOF >> ${HOME}/.config/bspwm/bspwmrc
+cat << EOF >> ~/.config/bspwm/bspwmrc
 
-${HOME}/.fehbg &
-xrdb ${HOME}/.Xresources &
-${HOME}/.config/polybar/launch.sh &
+~/.fehbg &
+xrdb ~/.Xresources &
+~/.config/polybar/launch.sh &
 setxkbmap br &
 picom -f &
 xsetroot -cursor_name left_ptr &
-${HOME}/scripts/dualbsp.sh
-EOF 
+~/scripts/dualbsp.sh
+EOF
