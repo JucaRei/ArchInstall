2a305d10508e1060d2777b523a9c855db7cdb58b
Reinaldo <reinaldo@gmail.com>
2022-04-07 19:27:55 -0300

    Fix

--- Artix/Runit/UEFI-Artix-Runit.sh
+++ Artix/Runit/UEFI-Artix-Runit.sh
@@ -14,39 +14,12 @@ echo "::1       localhost" >>/etc/hosts
 echo "127.0.1.1 artixnitro.localdomain artixnitro" >>/etc/hosts
 echo root:200291 | chpasswd
 
-cat <<EOF >/etc/sudoers
-## sudoers file.
-##
-## This file MUST be edited with the 'visudo' command as root.
-## Failure to use 'visudo' may result in syntax or file permission errors
-## that prevent sudo from running.
-##
-## See the sudoers man page for the details on how to write a sudoers file.
-##
+pacman -Sy artix-archlinux-support --noconfirm
+pacman -Sy artix-keyring --noconfirm
+# pacman -Sy lib32-artix-archlinux-support --noconfirm
+pacman-key --populate artix
 
-Defaults passwd_timeout=0
-Defaults timestamp_timeout=15
-
-##
-## User privilege specification
-##
-root ALL=(ALL) ALL
-
-## Uncomment to allow members of group wheel to execute any command
-%wheel ALL=(ALL) ALL
-
-## Same thing without a password
-%wheel ALL=(ALL) NOPASSWD: ALL
-
-## Run some commands without a password
-%wheel ALL=(ALL) NOPASSWD: /usr/bin/pacman -Sy
-%wheel ALL=(ALL) NOPASSWD: /usr/bin/pacman -Syuw
-%wheel ALL=(ALL) NOPASSWD: /usr/bin/pacman -Syuw --noconfirm
-%wheel ALL=(ALL) NOPASSWD: /usr/bin/pacman -Syyuw --noconfirm
-
-## Read drop-in files from /etc/sudoers.d
-@includedir /etc/sudoers.d
-EOF
+pacman-key --populate archlinux
 
 # Enable pacman Color
 sed -i '/Color/s/^#//' /etc/pacman.conf
@@ -54,18 +27,13 @@ sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
 
 # Enable multilib repo
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+sed -i "/\[lib32\]/,/Include/"'s/^#//' /etc/pacman.conf
 
 # Setting package signing option to require signature
 sed -i '/\[core\]/a SigLevel\ =\ PackageRequired' /etc/pacman.conf
 sed -i '/\[multilib\]/a SigLevel\ =\ PackageRequired' /etc/pacman.conf
 sed -i '/\[community\]/a SigLevel\ =\ PackageRequired' /etc/pacman.conf
 sed -i '/\[extra\]/a SigLevel\ =\ PackageRequired' /etc/pacman.conf
-
-pacman -S artix-archlinux-support --noconfirm
-pacman -S artix-keyring --noconfirm
-pacman-key --populate artix
-
-pacman-key --populate archlinux
 
 # ADD Repos
 cat <<EOF >>/etc/pacman.conf
@@ -123,11 +91,18 @@ pacman-key --lsign-key 9AE4078033F8024D
 
 cat <<EOF >>/etc/pacman.conf
 
+[omniverse]
+Server = http://omniverse.artixlinux.org/$arch
+
+#[universe]
+#Server = https://universe.artixlinux.org/$arch
+#Server = https://mirror1.artixlinux.org/universe/$arch
+#Server = https://mirror.pascalpuffke.de/artix-universe/$arch
+#Server = https://artixlinux.qontinuum.space:4443/artixlinux/universe/os/$arch
+#Server = https://mirror1.cl.netactuate.com/artix/universe/$arch
+
 #[liquorix]
 #Server = https://liquorix.net/archlinux/$repo$arch
-
-[universe]
-Server = https://universe.artixlinux.org/$arch
 
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist
@@ -140,6 +115,10 @@ tmpfs /tmp tmpfs noatime,mode=1777 0 0
 EOF
 
 pacman -Syyw
+
+sleep 3
+
+pacman -Sy
 
 pacman -S grub grub-btrfs efibootmgr mesa mesa-utils backlight-runit networkmanager preload reflector nfs-utils nfs-utils-runit samba samba-runit metalog metalog-runit mpd mpd-runit networkmanager-runit network-manager-applet dropbear dropbear-runit powertop thermald thermald-runit htop neofetch chrony chrony-runit dialog duf bat exa rsm avahi avahi-runit xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-runit bluez-utils pulseaudio-bluetooth pulseaudio-alsa pulseaudio-equalizer pulseaudio-jack alsa-utils alsa-utils-runit bash-completion exfat-utils cups cups-runit hplip rsync rsync-runit acpi acpid acpi_call-dkms virt-manager libvirt-runit qemu qemu-guest-agent-runit qemu-arch-extra vde2 edk2-ovmf bridge-utils dnsmasq dnsmasq-runit vde2 ebtables openbsd-netcat iptables-nft ipset firewalld firewalld-runit flatpak nss-mdns acpid-runit os-prober ntfs-3g
 
@@ -196,12 +175,14 @@ EOF
 # Power top
 cat <<EOF >>/etc/rc.local
 # PowerTop
-echo 60000 > /sys/bus/usb/devices/2-1.5/power/autosuspend_delay_ms
-echo 60000 > /sys/bus/usb/devices/2-1.6/power/autosuspend_delay_ms
-echo 60000 > /sys/bus/usb/devices/3-1.5/power/autosuspend_delay_ms
-echo 60000 > /sys/bus/usb/devices/3-1.6/power/autosuspend_delay_ms
-echo 60000 > /sys/bus/usb/devices/4-1.5/power/autosuspend_delay_ms
-echo 60000 > /sys/bus/usb/devices/4-1.6/power/autosuspend_delay_ms
+powertop --auto-tune
+
+# echo 60000 > /sys/bus/usb/devices/2-1.5/power/autosuspend_delay_ms
+# echo 60000 > /sys/bus/usb/devices/2-1.6/power/autosuspend_delay_ms
+# echo 60000 > /sys/bus/usb/devices/3-1.5/power/autosuspend_delay_ms
+# echo 60000 > /sys/bus/usb/devices/3-1.6/power/autosuspend_delay_ms
+# echo 60000 > /sys/bus/usb/devices/4-1.5/power/autosuspend_delay_ms
+# echo 60000 > /sys/bus/usb/devices/4-1.6/power/autosuspend_delay_ms
 
 # Preload
 preload
@@ -265,6 +246,42 @@ useradd -m junior
 echo junior:200291 | chpasswd
 usermod -aG libvirt junior
 
+echo "junior ALL=(ALL) ALL" >>/etc/sudoers.d/junior
+
+cat <<EOF >/etc/sudoers
+## sudoers file.
+##
+## This file MUST be edited with the 'visudo' command as root.
+## Failure to use 'visudo' may result in syntax or file permission errors
+## that prevent sudo from running.
+##
+## See the sudoers man page for the details on how to write a sudoers file.
+##
+
+Defaults passwd_timeout=0
+Defaults timestamp_timeout=15
+
+##
+## User privilege specification
+##
+root ALL=(ALL) ALL
+
+## Uncomment to allow members of group wheel to execute any command
+%wheel ALL=(ALL) ALL
+
+## Same thing without a password
+%wheel ALL=(ALL) NOPASSWD: ALL
+
+## Run some commands without a password
+%wheel ALL=(ALL) NOPASSWD: /usr/bin/pacman -Sy
+%wheel ALL=(ALL) NOPASSWD: /usr/bin/pacman -Syuw
+%wheel ALL=(ALL) NOPASSWD: /usr/bin/pacman -Syuw --noconfirm
+%wheel ALL=(ALL) NOPASSWD: /usr/bin/pacman -Syyuw --noconfirm
+
+## Read drop-in files from /etc/sudoers.d
+@includedir /etc/sudoers.d
+EOF
+
 # sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
 # sed -i 's/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers
 # # sed -i 's/# %sudo ALL=(ALL:ALL) ALL/%sudo ALL=(ALL:ALL) ALL/g' /etc/sudoers
@@ -278,17 +295,23 @@ usermod -aG libvirt junior
 # %wheel ALL=(ALL) NOPASSWD: /usr/bin/pacman -Syyuw --noconfirm
 # EOF
 
-echo "junior ALL=(ALL) ALL" >>/etc/sudoers.d/junior
-
 # pacman -Rs linux acpi_call --noconfirm
 # pacman -S acpi_call-lts --noconfirm
 
+touch /etc/modprobe.d/i915.conf
+cat <<EOF >/etc/modprobe.d/i915.conf
+options i915 enable_guc=2 enable_dc=4 enable_hangcheck=0 error_capture=0 enable_dp_mst=0 fastboot=1
+EOF
+
 sudo sed -i 's/#GRUB_DISABLE_OS_PROBER=true/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
 # sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=2 quiet udev.log_level=0 i915.fastboot=1 i915.enable_guc=2 acpi_backlight=vendor console=tty2 zswap.enabled=1 zswap.compressor=zstd zswap.max_pool_percent=10 zswap.zpool=zsmalloc mitigations=off nowatchdog msr.allow_writes=on pcie_aspm=force module.sig_unenforce intel_idle.max_cstate=1 i915.enable_dc=0 ahci.mobile_lpm_policy=1 cryptomgr.notests initcall_debug nvidia-drm.modeset=1 intel_iommu=on,igfx_off net.ifnames=0 no_timer_check noreplace-smp page_alloc.shuffle=1 rcupdate.rcu_expedited=1 tsc=reliable"/g' /etc/default/grub
-sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=2 quiet udev.log_level=0 i915.fastboot=1 i915.enable_guc=2 acpi_backlight=vendor console=tty2 zswap.enabled=1 zswap.compressor=zstd zswap.max_pool_percent=10 zswap.zpool=zsmalloc mitigations=off nowatchdog msr.allow_writes=on pcie_aspm=force module.sig_unenforce intel_idle.max_cstate=1 ahci.mobile_lpm_policy=1 cryptomgr.notests initcall_debug nvidia-drm.modeset=1 intel_iommu=on,igfx_off net.ifnames=0 no_timer_check noreplace-smp page_alloc.shuffle=1 rcupdate.rcu_expedited=1 tsc=reliable"/g' /etc/default/grub
+# sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=2 quiet udev.log_level=0 i915.fastboot=1 i915.enable_guc=2 acpi_backlight=vendor console=tty2 zswap.enabled=1 zswap.compressor=zstd zswap.max_pool_percent=10 zswap.zpool=zsmalloc mitigations=off nowatchdog msr.allow_writes=on pcie_aspm=force module.sig_unenforce intel_idle.max_cstate=1 ahci.mobile_lpm_policy=1 cryptomgr.notests initcall_debug nvidia-drm.modeset=1 intel_iommu=on,igfx_off net.ifnames=0 no_timer_check noreplace-smp page_alloc.shuffle=1 rcupdate.rcu_expedited=1 tsc=reliable"/g' /etc/default/grub
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=2 quiet udev.log_level=0 acpi_backlight=vendor console=tty2 zswap.enabled=1 zswap.compressor=zstd zswap.max_pool_percent=10 zswap.zpool=zsmalloc mitigations=off nowatchdog msr.allow_writes=on pcie_aspm=force module.sig_unenforce intel_idle.max_cstate=1 ahci.mobile_lpm_policy=1 cryptomgr.notests initcall_debug nvidia-drm.modeset=1 intel_iommu=on,igfx_off net.ifnames=0 no_timer_check noreplace-smp page_alloc.shuffle=1 rcupdate.rcu_expedited=1 tsc=reliable"/g' /etc/default/grub
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # i915.fastboot=1
+
+paccache -rk0
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
