fa6726169206bb654aef83004143ba0143074e5b
Reinaldo <reinaldo800@gmail.com>
2022-05-23 12:14:41 -0300

    Fix

--- WM-Install-Scripts/Bspwm-Artix.sh
+++ WM-Install-Scripts/Bspwm-Artix.sh
@@ -20,7 +20,7 @@ sudo pacman -Syy
 # Xorg Packages
 paru -S xorg-server xorg-font-util xorg-fonts-encodings xorg-fonts-alias-misc xorg-fonts-misc xorg-setxkbmap xorg-xauth xorg-mkfontscale xorg-xsetroot xorg-xinit libxrandr libxft libinput libinput-gestures xorg-xrdb libxinerama xorg-xbacklight xorg-xcursorgen xorg-xdpyinfo xorg-xdriinfo xorg-xev xorg-xhost xorg-xkbcomp xorg-xkbevd xorg-xkbutils xorg-xkill xorg-xlsclients xorg-xmodmap xorg-xprop xorg-xset xorg-xsetroot xorg-xvinfo xorg-xwininfo
 
-paru -S bspwm arandr numlockx firefox-esr-bin glow sxhkd wmctrl polybar rofi-calc flatpak light-locker playerctl lua dmenu nitrogen feh unclutter picom-ibhagwan-git zathura neovim rofi dunst scrot archlinux-wallpaper lxappearance lightdm lightdm-runit web-greeter lightdm-settings light-locker ncmpcpp mpc neofetch htop geany
+paru -S bspwm arandr numlockx firefox-bin glow sxhkd wmctrl polybar rofi-calc flatpak light-locker playerctl lua dmenu nitrogen feh unclutter picom-ibhagwan-git zathura neovim rofi dunst scrot archlinux-wallpaper lxappearance lightdm lightdm-runit web-greeter lightdm-settings light-locker ncmpcpp mpc neofetch htop geany
 
 # Terminals
 paru -S xfce4-terminal kitty rxvt-unicode-truecolor-wide-glyphs
@@ -32,25 +32,33 @@ chmod -R 640 ~/.config/libfm/libfm.conf
 # paru -S nemo nemo-audio-tab nemo-emblems nemo-fileroller nemo-image-converter nemo-pastebin nemo-preview nemo-seahorse nemo-share nemo-terminal folder-color-nemo nemo-compare
 
 touch $HOME/.config/starship.toml
+mkdir -p $HOME/Documents/workspace/{Github,Builds,Others,Customizations,Configs,Tests,Composes}
 mkdir -p ~/.urxvt/ext
 mkdir -p ~/.conky
 mkdir -p ~/.ncmpcpp
+mkdir -p ~/.config/mpd
 mkdir -p ~/.runit
 mkdir -p $HOME/.bin
 mkdir -p $HOME/local/.bin
+mkdir -p $HOME/local/share/fonts
 mkdir -p $HOME/local/share/applications
 mkdir -p $HOME/.config/{conky,bspwm,mpd,sxhkd,picom,dunst,rofi,mpv,zathura}
-mkdir -pv ~/Pictures/Wallpapers
+# mkdir -pv ~/Pictures/Wallpapers
 cd ~/.config/mpd
 touch database mpd.conf mpd.fifo mpd.log mpdstate
+
+cd ~/Documents/workspace/Configs
+git clone --depth 1 https://github.com/JucaRei/ArchInstall
+cd /Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro-Artix/
+cp -rf /home/junior/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro-Artix/bashrc ~/.bashrc
+cd 
+cp -rf ~/Documents/workspace/Configs/ArchInstall/wallpapers ~/Pictures/Wallpapers
 
 cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/mpd/mpd.conf ~/.config/mpd
 cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/ncmpcpp/config ~/.ncmpcpp
 
 cd
 cp -f ~/Documents/workspace/Configs/ArchInstall/wallpapers ~/Pictures/Wallpapers
-cp -rf ~/Documents/workspace/Configs/ArchInstall/Dotw-WM/Bspwm-Nitro/polybar ~/.config
-cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Xresources ~/.Xresources
 cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/fehauto.sh ~/.local/bin
 cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/xprofile-vm ~/.xprofile
 cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/rxvt/vtwhell ~/.urxvt/ext
@@ -58,14 +66,28 @@ cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/rxvt/config-
 cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/dualbsp-VM.sh ~/.bin
 cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/DualMonPolybar-VM.sh ~/.bin
 sudo cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/Dual-DM-VM.sh /etc/lightdm
-cp -f /etc/dunst/dunstrc ~/.config/dunst/
+cp -f /etc/dunst/ ~/.config/dunst/
 
-install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
-install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
-# cp $HOME/ArchInstall/bspwm-old/bspwmrc $HOME/.config/bspwm/
-# cp $HOME/ArchInstall/bspwm-old/sxhkdrc $HOME/.config/sxhkd/
-
-cp /etc/dunst/dunstrc $HOME/.config/dunst
+# install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
+# install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/polybar/ ~/.config/
+chmod +x ~/.config/polybar/launch.sh
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/starship/starship2.toml ~/.config/starship.toml
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/Xresources ~/.Xresources
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/zathura/ ~/.config/
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/sxhkd/ ~/.config/
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/picom/ ~/.config/
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/themes/ ~/.themes/
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/icons/ ~/.icons/
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/rofi/ ~/.config/
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/nano/ ~/.config/
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/mpv/ ~/.config/
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/conky/ ~/.conky/
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/bspwm/ ~/.config/
+chmod +x ~/.config/bspwm/bspwmrc
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/geany/ ~/.config/
+cp -f ~/Documents/workspace/Configs/ArchInstall/Dots-WM/Bspwm-Nitro/dunst ~/.config/
+chmod +x ~/.config/dunst/dunstrc
 
 #cp /usr/share/doc/bspwm/examples/bspwmrc .config/bswpm
 #cp /usr/share/doc/bspwm/examples/sxhkdrc .config/sxhkd
@@ -83,38 +105,10 @@ cd
 cd $HOME/Documents/workspace/Configs/ArchInstall/fonts/fonts && cp *.ttf *.otf $HOME/.local/share/fonts/
 fc-cache -fv
 
-#Rofi
-cd $HOME/Documents/workspace/Configs/rofi
-chmod +x setup.sh
-./setup.sh
 
-cd
-
-#POLYBAR
-#cd $HOME/Documents/workspace/Configs/polybar-themes
-#chmod +x setup.sh
-#./setup.sh
-#cd
-
-# sxhkdrc Config your terminal
-# picom conf remove vsync
-
-#cp /etc/X11/xinit/xinitrc .xinitrc
-#cp /etc/X11/xinit/xserverrc .xserverrc
-
-mkdir -p ~/.config/mpd && cd ~/.config/mpd
-touch database mpd.conf mpd.fifo mpd.log mpdstate
-
-cd
 
 cp -r $HOME/workspace/Configs/ArchInstall/Dots-WM/mpd/mpd.conf $HOME/.config/mpd/
-cp -r $HOME/workspace/Configs/ArchInstall/Dots-WM/ncmpcpp/config $HOME/.config/ncmpcpp/
-
-###   POLYBAR
-# mv dotfiles/polybar $HOME/.config
-# mv dotfiles/Xresources $HOME/.Xresources
-
-# sudo ln -s /etc/runit/sv/lightdm /run/runit/service
+cp -r $HOME/workspace/Configs/ArchInstall/Dots-WM/ncmpcpp/config $HOME/.ncmpcpp/
 
 printf "\e[1;32mCHANGE NECESSARY FILES BEFORE REBOOT\e[0m"
 printf "\e[1;32mLIGHTDM CONFIG (SEAT: GREETER-SESSION TO lightdm-slick greeter)\e[0m"
