92a9ec0bd81a9ea1dd21db45bc116aa726e8c660
Reinaldo <reinaldo800@gmail.com>
2023-01-17 18:51:03 -0300

    more packages

--- Debian/debian.sh
+++ Debian/debian.sh
@@ -75,20 +75,25 @@ mount -t vfat -o noatime,nodiratime /dev/sda1 /mnt/boot/efi
 # debootstrap --include "bash,zsh,wpasupplicant,locales,grub2,wget,curl,ntp,network-manager,dhcpcd5,linux-image-amd64,firmware-linux-free" --arch amd64 chimaera /mnt http://devuan.c3sl.ufpr.br/merged/ chimaera
 # debootstrap --include "bash,zsh,iwd,locales,grub2,wget,curl,ntp,network-manager,dhcpcd5,linux-image-amd64,firmware-linux-free" --arch amd64 chimaera /mnt http://devuan.c3sl.ufpr.br/merged/ chimaera
 # debootstrap --arch amd64 chimaera /mnt http://devuan.c3sl.ufpr.br/merged/ chimaera
-debootstrap --variant=minbase --include=apt,apt-utils,cpio,cron,debconf-i18n,debian-archive-keyring,zstd,locales,btrfs-progs,dmidecode,kmod,less,fdisk,gpgv,neovim,ncurses-base,netbase,procps,systemd,systemd-sysv,udev,ifupdown,init,iproute2,iputils-ping,bash,whiptail,ca-certificates --arch amd64 bullseye /mnt http://debian.c3sl.ufpr.br/debian/ bullseye
+debootstrap --variant=minbase --include=apt,apt-utils,cpio,cron,console-setup,dosfstools,keyboard-configuration,debian-archive-keyring,zstd,locales,btrfs-progs,dmidecode,kmod,less,gdisk,gpgv,neovim,ncurses-base,netbase,procps,systemd,systemd-sysv,udev,ifupdown,init,iproute2,iputils-ping,bash,whiptail,ca-certificates --arch amd64 bullseye /mnt http://debian.c3sl.ufpr.br/debian/ bullseye
 # deb http://debian.c3sl.ufpr.br/debian/ main contrib non-free
 
+# apt install --yes console-setup locales chrony dosfstools wget dracut efitools efibootmgr sbsigntool python3 tpm2-tools linux-image-amd64 linux-doc systemd-boot systemd-boot-efi mokutil gdisk
+
 # whiptail or dialog 
-# tasksel-data
+# tasksel-data, debconf-i18n
 
 
-ca-certificates
+# ca-certificates
 
 # Mount points
 for dir in dev proc sys run; do
         mount --rbind /$dir /mnt/$dir
         mount --make-rslave /mnt/$dir
 done
+
+# Config neovim as default editor
+chroot /mnt update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 100
 
 # Desabilita instalar recomendados
 touch /mnt/etc/apt/apt.conf
@@ -201,8 +206,11 @@ chroot /mnt apt install pipewire libspa-0.2-bluetooth libspa-0.2-jack pipewire-a
 touch /mnt/etc/pipewire/media-session.d/with-pulseaudio
 cp /mnt/usr/share/doc/pipewire/examples/systemd/user/pipewire-pulse.* /mnt/etc/systemd/user/
 
+# ssh
+chroot /mnt apt install openssh-client openssh-server --no-install-recommends -y
+
 # Utils
-chroot /mnt apt install dracut manpages debian-keyring build-essential grub-efi-amd64 os-prober wget curl sysfsutils chrony network-manager iwd linux-image-amd64 linux-headers-amd64 firmware-linux --no-install-recommends -y
+chroot /mnt apt install dracut manpages debian-keyring build-essential grub-efi-amd64 efibootmgr os-prober wget curl sysfsutils chrony network-manager iwd linux-image-amd64 linux-headers-amd64 firmware-linux --no-install-recommends -y
 # aptitude initramfs-tools
 # dracut --list-modules --kver 5.10.0-20-amd64
 
@@ -211,27 +219,94 @@ hostonly="yes"
 hostonly_cmdline=no
 dracutmodules+=" bash systemd kernel-modules rootfs-block btrfs udev-rules resume usrmount base fs-lib shutdown "
 use_fstab=yes
+# add_drivers+=" crc32c-intel btrfs i915 ahci nvidia nvidia_drm nvidia_uvm nvidia_modeset "
 add_drivers+=" crc32c-intel btrfs i915 nvidia nvidia_drm nvidia_uvm nvidia_modeset "
 force_drivers+=" z3fold "
 omit_dracutmodules+=" i18n luks rpmversion lvm fstab-sys lunmask fstab-sys securityfs img-lib biosdevname caps crypt crypt-gpg dmraid dmsquash-live mdraid "
 show_modules="yes"
+do_prelink=no
 # compress="cat";
-nofscks="yes"
-compress="zstd"
-no_host_only_commandline="yes"
+nofscks=yes
+#compress="zstd"
+compress="lz4 -l -9"
+no_host_only_commandline=yes
+EOF
+
+# Early micro code
+cat <<EOF >/mnt/etc/dracut.conf.d/intel_ucode.conf
+early_microcode=yes
+EOF
+
+cat <<EOF >/mnt/etc/dracut.conf.d/10-zram.conf
+# add_drivers+=" zram "
+EOF
+
+cat <<EOF >/mnt/etc/dracut.conf.d/10-lz4.conf
+add_drivers+=" lz4hc lz4hc_compress "
+EOF
+
+# Load Modules on early-boot
+mkdir -pv /mnt/etc/modprobe.d
+touch /mnt/etc/modprobe.d/bbswitch.conf
+cat <<EOF >/mnt/etc/modprobe.d/bbswitch.conf
+#options bbswitch load_state=0 unload_state=1 
+EOF
+
+#Compress
+mkdir -pv /mnt/etc/sysctl.d
+cat <<EOF >/mnt/etc/sysctl.d/00-swap.conf
+vm.vfs_cache_pressure=500
+vm.swappiness=100
+vm.dirty_background_ratio=1
+vm.dirty_ratio=50
+EOF
+
+cat <<EOF >/mnt/etc/sysctl.d/00-intel.conf
+# Intel Graphics
+dev.i915.perf_stream_paranoid=0
+EOF
+
+# Touchpad tap to click
+mkdir -pv /mnt/etc/X11/xorg.conf.d/
+touch /mnt/etc/X11/xorg.conf.d/30-touchpad.conf
+cat <<EOF >/mnt/etc/X11/xorg.conf.d/30-touchpad.conf
+Section "InputClass"
+        # Identifier "SynPS/2 Synaptics TouchPad"
+        # Identifier "SynPS/2 Synaptics TouchPad"
+        # MatchIsTouchpad "on"
+        # Driver "libinput"
+        # Option "Tapping" "on"
+
+        Identifier      "touchpad"
+        Driver          "libinput"
+        MatchIsTouchpad "on"
+        Option          "Tapping"       "on"
+EndSection
 EOF
 
 # Tools
 chroot /mnt apt install bash-completion bzip2 man-db gdisk dosfstools mtools p7zip neofetch fzf bat duf --no-install-recommends -y
 
 # Optimizations
-chroot /mnt apt install earlyoom powertop thermald irqbalance --yes
+chroot /mnt apt install earlyoom powertop thermald irqbalance --no-install-recommends -y
 
 
-zsh stterm rxvt-unicode-256color
+# zsh stterm rxvt-unicode-256color
 
 # Microcode
 chroot /mnt apt install intel-microcode --no-install-recommends -y
+
+# intel Hardware Acceleration
+chroot /mnt apt install intel-media-driver-non-free --no-install-recommends -y
+
+# Nvidia Drivers
+chroot /mnt apt install nvidia-driver libnvcuvid1 libnvidia-encode1 firmware-misc-nonfree --no-install-recommends -y
+
+# Minimal xorg packages
+chroot /mnt apt install xserver-xorg-core xserver-xorg-video-intel xserver-xorg-input-evdev x11-xserver-utils x11-xkb-utils x11-utils xinit --no-install-recommends -y
+
+# Infrastructure packages
+chroot /mnt apt install ansible virt-manager bridge-utils qemu qemu-ga qemu-user-static qemuconf podman podman-compose binfmt-support containers.image buildah slirp4netns cni-plugins fuse-overlayfs --no-install-recommends -y
 
  
 # Umount
@@ -254,9 +329,10 @@ chroot /mnt echo "America/Sao_Paulo" > /mnt/etc/timezone && \
                 dpkg-reconfigure -f noninteractive tzdata && \
                 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
                 sed -i -e 's/# pt_BR.UTF-8 UTF-8/pt_BR.UTF-8 UTF-8/' /etc/locale.gen && \
-                echo 'LANGUAGE="en_US.UTF-8"'>/etc/default/locale && \
+                echo 'LANGUAGE="en_US.UTF-8"'>/mnt/etc/default/locale && \
                 export LANGUAGE=en_US.UTF-8 && \
                 export LC_ALL=en_US.UTF-8 && \
+                echo 'KEYMAP="br-abnt2"' > /etc/vconsole.conf && \ 
                 dpkg-reconfigure --frontend=noninteractive locales && \
                 update-locale LANG=en_US.UTF-8 && \
                 localedef -i en_US -f UTF-8 en_US.UTF-8
@@ -308,3 +384,5 @@ chroot /mnt systemctl enable earlyoom.service
 chroot /mnt systemctl enable powertop.service 
 chroot /mnt systemctl enable thermald.service 
 chroot /mnt systemctl enable irqbalance.service
+
+# chroot /mnt dracut --regenete-all --force --hostonly --kver 5.10.0-20-amd64
