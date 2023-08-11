bd3af47f5393333577aef7ac446cfc28b4b6f996
Reinaldo <reinaldo800@gmail.com>
2023-08-11 16:23:42 -0300

    fix

--- Distros/Debian/desktops/kde.sh
+++ Distros/Debian/desktops/kde.sh
@@ -2,7 +2,7 @@
 
 apt install --no-install-recommends -y kde-config-plymouth kde-config-screenlocker plasma-discover \
 kde-config-sddm xdg-utils xdg-user-dirs xdg-desktop-portal-kde systemsettings kde-plasma-desktop  \
-plasma-desktop plasma-workspace plasma-integration plasma-pa plasma-nm firefox kate arc kcalc \
+plasma-desktop plasma-workspace plasma-integration plasma-pa plasma-nm firefox-esr kate arc kcalc \
 plasma-discover-backend-snap plasma-discover-backend-flatpak kde-config-flatpak kwin-x11 kde-config-gtk-style \
 kio-extras qml-module-qtbluetooth plasma-discover-backend-fwupd kde-spectacle okular powerdevil bluedevil sddm \
 qml-module-org-kde-newstuff schedtool kwalletmanager ark kscreen libcanberra-pulse qt5-style-kvantum qt5-style-kvantum-themes \
