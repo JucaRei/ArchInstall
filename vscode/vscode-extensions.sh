526bdbcecb348d1fae357a252e1291b6f968d584
Reinaldo <reinaldo800@gmail.com>
2024-02-03 18:39:33 -0300

    utils

---
+++ vscode/vscode-extensions.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# code --list-extensions
+
+code --install-extension --force arrterian.nix-env-selector 
+code --install-extension --force bbenoist.nix 
+code --install-extension --force brettm12345.nixfmt-vscode 
+code --install-extension --force jeff-hykin.better-nix-syntax 
+code --install-extension --force jnoortheen.nix-ide  
+code --install-extension --force joaopalmeiro.icons-octicons 
+code --install-extension --force miguelsolorio.symbols 
+code --install-extension --force monokai.theme-monokai-pro-vscode 
+code --install-extension --force oderwat.indent-rainbow 
+code --install-extension --force pkief.material-product-icons
